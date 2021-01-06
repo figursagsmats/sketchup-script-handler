9f58cc1e1b515a9eb13b8e19d0c72fecfb4ea3f1
Gustav Lagnström <gustav.lagnstrom@gmail.com>
2021-01-06 18:33:29 +0100

    Refactor the whole whing to make it easier to use

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # sketchup-script-handler
+1. Download scripthandler.rb and place it in your plugins-folder. That way is gets load when you start SketchUp.
+2. Use another file (existing or new) in your plugins-folder to to consume the ScriptHandler-module like this:
+```
+ScriptHandler.add_multi_file_script("C:/path/to/your/main/script.rb")
+```
--- scripthandler.rb
+++ scripthandler.rb
@@ -1,155 +1,79 @@
-require 'sketchup.rb'
-SKETCHUP_CONSOLE.clear
-
 module ScriptHandler
-
-    @@scripts = []
-    class DirScanner
-        def initialize(path)
-            puts "initialize"
-            @extra_plugin_path = path
-            @sem = @extra_plugin_path+"/.sem"
+    class ScriptFileWatcher
+        def initialize(script_path, is_single_file=false)
+            @is_single_file = is_single_file
+            @dir_path = File.dirname(script_path)
+            @script_path = script_path
+            @prev_ruby_files = []
+            @main_script = script_path.split("/").last # /some/path/to/script/main.rb => main.rb
+            main_script_name = @main_script.split(".").first #eg main.rb => main
+            
+            #sem is just using a file as a variable for last modified time
+            if (is_single_file)
+                @sem = @dir_path + "/." + main_script_name + ".sem"
+            else
+                @sem = @dir_path + "/.sem"
+                @pattern = @dir_path + "/**/*.rb"
+            end
+            start_scanning()
         end
 
-        def doload(checksem=false)
-            pattern = "#{@extra_plugin_path}/*.rb"
-            Dir[pattern].each() { |x|
-                if (!checksem) or (File.mtime(x) > File.mtime(@sem))
-                    touch
+        def find_all_ruby_files()
+
+            if (@is_single_file)
+                all_ruby_files = [@script_path]
+            else
+                all_ruby_files = Dir.glob(@dir_path + "/**/*.rb")
+            end
+        end
+
+        def check_all_files()
+            all_ruby_files = find_all_ruby_files()
+
+            #Has the list of files changed?
+            diff = all_ruby_files - @prev_ruby_files
+            unless(diff.empty?)
+                puts "Added the following files to the watch list:"
+                all_ruby_files.each() { |x| puts x }
+                puts "The complete watch list is now:"
+                all_ruby_files.each() { |x| puts x }
+            end
+
+            @prev_ruby_files = all_ruby_files
+
+            all_ruby_files.each() { |x|
+                if (File.mtime(x) > File.mtime(@sem))
+                    update_last_modified_time()
+                    puts "Reloading #{x}..."
                     load(x)
-                    puts "(Re-)loaded #{x}"
+                    puts "Done reloading #{x}!"
                 end
             }
             nil
         end
 
-        def touch
-           File.new(@sem, "w").close()
-        end
-        
-        def start_scanning
-            puts "Scanning started"
-            doload
-            @timer_id = UI::start_timer(1, true) {
-                doload(true)
-            }
-        end
-        def stop_scanning()
-            UI.stop_timer(@timer_id)
-        end
-    end
-
-    class SketchupScript
-
-        def initialize(root_path, entry_point_script, should_reload)
-            @root_path = root_path
-            add_entry_point_script(entry_point_script)
-            @should_reload = should_reload
-            @additional_reloads = []
-            @scanner = DirScanner.new(root_path)
-            self.start_watching_for_changes()
-            self.require_main_script()
-        end
-
-        def start_watching_for_changes()
-            #TODO make dirscanner work for files in additional_reloads instead of whole directorty
-            @scanner.start_scanning()
-        end
-
-        def stop_watching_for_changes()
-            @scanner.stop_scanning()
-        end
-
-        def require_main_script
-            require @entry_point_script
-        end
-
-        def reload
-            if @should_reload then
-                puts "reloading #{@entry_point_script}..."
-                load @entry_point_script
-
-                for script in @additional_reloads do
-                    puts "reloading #{script}..."
-                    load script
-                end
-            end
-        end
-
-        def doload
-            for script in @additional_reloads do
-
-                if (!checksem) or (File.mtime(script) > File.mtime(@sem))
-                    touch
-                    load(x)
-                    puts "(Re-)loaded #{script}"
-                end
-            end
-        end
-        def touch
+        def update_last_modified_time
             File.new(@sem, "w").close()
         end
 
-        def watch_for_changes
-            doload
-            UI::start_timer(1, true) {
-               doload(true)
+        def start_scanning
+            #Starting timer that checks all ruby files for changes
+            @timer_id = UI::start_timer(1, true) {
+                check_all_files()
             }
         end
 
-        def add_entry_point_script(path)
-            path = make_absolute(path)
-            @entry_point_script = path
-            puts "Setting #{path} as main script entry point"
-
+        def stop_scanning() #No use for this yet
+            UI.stop_timer(@timer_id)
         end
 
-        def add_folder_to_reload(path,include_subfolders)
-            path = make_absolute(path)
-            folders = include_subfolders ? '**' : '*'
-            pattern = File.join(path, folders, '*.rb')
-            Dir.glob(pattern).each { |filename|
-                #@additional_reloads.push(filename)
-                puts "added #{filename} to list of scripts to reload"
-            }
-        end
-
-        def add_script_to_reload(path)
-            script_path = make_absolute(path)
-            @additional_reloads.push(script_path)
-            puts "added #{script_path} to list of scripts to reload"
-
-        end
-
-        def make_absolute(path)
-            if path.include?(@root_path) then 
-                return path
-            else
-                return File.join(@root_path,path)
-            end
-        end
     end
 
-    def self.reload()
-        puts "Reloading scripts..."
-        for script in @@scripts do
-            script.reload()
-        end
-        puts "Finished reloading."
-    end
-
-    def self.stop_watching_for_script_changes()
-        for script in @@scripts do
-            script.stop_watching_for_changes()
-        end
-        puts "Stopped looking for changes scripts..."
-    end
-
-    def self.add_script(folder,script_file_name,should_reload)
-        script = SketchupScript.new(folder,script_file_name,should_reload)
-        @@scripts.push(script)
+    def self.add_single_file_script(script_path)
+        ScriptFileWatcher.new(script_path, true)
     end
     
+    def self.add_multi_file_script(script_path)
+        ScriptFileWatcher.new(script_path, false)
+    end
 end
-
-
